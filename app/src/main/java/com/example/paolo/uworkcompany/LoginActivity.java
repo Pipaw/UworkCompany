158e9fc53b95aaaedc1c23f3a8f6903bd24abce4
Paolo <paolodecastr06@gmail.com>
2018-12-27 17:49:33 +0800

    (Firebase)Login Functionality Done!

--- app/src/main/java/com/example/paolo/uworkcompany/LoginActivity.java
+++ app/src/main/java/com/example/paolo/uworkcompany/LoginActivity.java
@@ -1,13 +1,21 @@
 package com.example.paolo.uworkcompany;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,6 +23,7 @@ public class LoginActivity extends AppCompatActivity {
     private AutoCompleteTextView mEmailText;
     private EditText mPasswordText;
     private TextView mTextView;
+    private Button mButton;
 
     private FirebaseAuth mAuth;
 
@@ -23,10 +32,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Connecting member variable to the assets
         mEmailText = findViewById(R.id.loginEmailText);
         mPasswordText = findViewById(R.id.loginPasswordText);
         mTextView = findViewById(R.id.signUpText);
+        mButton = findViewById(R.id.loginButton);
 
+
+        //Register textview onClick method
         mTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,9 +48,50 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(mIntent);
             }
         });
-
+        //Authentication Initialization
         mAuth = FirebaseAuth.getInstance();
+    }
+    //Click and other Events
 
+    public void login(View view){
+        attemptLogin();
     }
 
+
+
+    //Own functionality
+
+    private void attemptLogin(){
+
+        String email = mEmailText.getText().toString();
+        String password = mPasswordText.getText().toString();
+
+        if(email.equals("") || password.equals(""))return;
+        Toast.makeText(this, "Login in progress...", Toast.LENGTH_SHORT).show();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("UworkComapny", "signInWithEmail() Complete "+ task.isSuccessful());
+                if(!task.isSuccessful()){
+                    Log.d("UworkCompany", "Problem Signing In " + task.getException());
+                    showDialogError("There was a Problem signing in");
+                }else {
+                    HomeActivity();
+                }
+            }
+        });
+    }
+
+    //Home Activity Intent
+    private void HomeActivity(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    private void showDialogError(String message){
+        new AlertDialog.Builder(this).setTitle("Error").setMessage(message).setPositiveButton
+                (android.R.string.ok, null).setIcon(android.R.drawable.ic_dialog_alert).show();
+    }
 }
