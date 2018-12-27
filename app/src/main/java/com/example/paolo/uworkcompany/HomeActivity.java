f7bb5d2dd1c3f4cd79c32074c6a5e54ce5f7ff96
Paolo <paolodecastr06@gmail.com>
2018-12-27 19:08:14 +0800

    -Home Activity Button

--- app/src/main/java/com/example/paolo/uworkcompany/HomeActivity.java
+++ app/src/main/java/com/example/paolo/uworkcompany/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.paolo.uworkcompany;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void findWorker(View view) {
+        // TODO: make a function to call new intent
+
     }
 }
